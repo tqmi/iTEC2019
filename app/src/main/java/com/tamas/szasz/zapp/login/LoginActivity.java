cc582fe3d5f63e21d6c278b45990c5dfe9cddde7
tqmi <45799508+tqmi@users.noreply.github.com>
2019-11-22 18:34:13 +0200

    strings for api urls added

--- app/src/main/java/com/tamas/szasz/zapp/login/LoginActivity.java
+++ app/src/main/java/com/tamas/szasz/zapp/login/LoginActivity.java
@@ -3,11 +3,13 @@ package com.tamas.szasz.zapp.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.TextureView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.tamas.szasz.zapp.R;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,8 +27,12 @@ public class LoginActivity extends AppCompatActivity {
 
         userEmail = (String) ((TextView)findViewById(R.id.login_email_tw)).getText();
         userPassword = (String) ((TextView)findViewById(R.id.login_password_tw)).getText();
-
-
+        URL authURL;
+        try {
+            authURL = new URL(R.string.auth_url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
 
     }
