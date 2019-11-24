7a12b27f6af1c16e474292c8032164b222a1349b
tqmi <45799508+tqmi@users.noreply.github.com>
2019-11-24 02:19:12 +0200

    added functionality to add chsrging stations

--- app/src/main/java/com/tamas/szasz/zapp/NavigationActivity.java
+++ app/src/main/java/com/tamas/szasz/zapp/NavigationActivity.java
@@ -76,6 +76,7 @@ import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.PopupWindow;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -125,6 +126,28 @@ public class NavigationActivity extends AppCompatActivity implements OnMapReadyC
         if(this.permissions) {
             addUserLocation();
         }
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+            Toast.makeText(this,
+                    marker.getTitle() +
+                            " has been clicked " + clickCount + " times.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 
     @Override
